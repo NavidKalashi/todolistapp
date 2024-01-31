05dd0c2c5deb98cb8ea213a219e9076a7dad3d4f
NavidKalashi <kalashinavid@gmail.com>
2024-01-31 19:12:18 +0330

    deatil page view

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='tasklist'),
+    path('', views.TaskList.as_view(), name='taskList'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='taskDetail'),
 ]
