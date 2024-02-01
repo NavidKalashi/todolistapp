86efa8f1fcc3b9d2c6a7d56423367f1a68d9be8b
NavidKalashi <kalashinavid@gmail.com>
2024-02-01 17:11:30 +0330

    create tasks

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.TaskList.as_view(), name='taskList'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='taskDetail'),
+    path('task-create/', views.TaskCreate.as_view(), name='taskCreate'),
 ]
