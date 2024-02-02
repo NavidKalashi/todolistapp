3fa830dc4321a4b4d750837c35284bbb84b08a59
NavidKalashi <kalashinavid@gmail.com>
2024-02-02 17:34:17 +0330

    create update and delete views

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.TaskList.as_view(), name='taskList'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='taskDetail'),
     path('task-create/', views.TaskCreate.as_view(), name='taskCreate'),
+    path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='taskUpdate'),
+    path('task-delete/<int:pk>/', views.TaskDelete.as_view(), name='taskDelete'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -19,4 +19,14 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('taskList')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('taskList')
+    
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('taskList')
