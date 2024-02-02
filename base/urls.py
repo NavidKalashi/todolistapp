b1b8f10d395a6d327ac6bf9e7c1f77f64556b2b1
NavidKalashi <kalashinavid@gmail.com>
2024-02-02 18:03:18 +0330

    add login and logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
     path('', views.TaskList.as_view(), name='taskList'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='taskDetail'),
     path('task-create/', views.TaskCreate.as_view(), name='taskCreate'),
